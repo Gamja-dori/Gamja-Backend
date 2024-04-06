d0a3fdaf099b41538233cd82f0b827ab5731d1c9
sforseohn <sforseohn@ewhain.net>
2024-04-06 12:23:12 +0900

    feat: add recommendation

--- dasi/recommend/models.py
+++ dasi/recommend/models.py
@@ -2,21 +2,20 @@ from django.db import models
 from users.models import EnterpriseUser
 from resume.models import Resume
 
-class RecommendResult(models.Model):
-    recommend_id = models.AutoField(primary_key=True)
+class SearchResult(models.Model):
+    id = models.AutoField(primary_key=True)
     query = models.CharField(max_length=255)
     user = models.ForeignKey(EnterpriseUser, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'recommend_results'
+        db_table = 'search_results'
 
 
 class RecommendedResume(models.Model):
-    recommended_resume_id = models.AutoField(primary_key=True)
+    recommend_result = models.ForeignKey(SearchResult, on_delete=models.CASCADE)
+    resume_id = models.ForeignKey(Resume, on_delete=models.CASCADE, default=-1)
     percentage = models.IntegerField()
     comment = models.CharField(max_length=255, null=True)
-    recommend_result = models.ForeignKey(RecommendResult, on_delete=models.CASCADE)
-    resume_id = models.ForeignKey(Resume, on_delete=models.CASCADE, default=-1)
 
     class Meta:
         db_table = 'recommended_resumes'
--- dasi/recommend/views.py
+++ dasi/recommend/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from .models import *
+from resume.models import Resume
+from users.models import SeniorUser
+from .recommendation import search
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from drf_yasg.utils import swagger_auto_schema
 
-# Create your views here.
+class SearchResultCreateView(APIView):
+    permission_classes = [AllowAny] 
+        
+    def create_search_result(self, data):
+        user_id = data.get("user_id")
+        query = data.get("query")
+        job_group = data.get("job_group")
+        job_role = data.get("job_role")
+        min_career_year = data.get("min_career_year")
+        skills = data.get("skills")
+        max_month_pay = data.get("max_month_pay")
+        commute_type = data.get("commute_type")
+        
+        # 검색 결과 (점수, 이력서 번호, 코멘트)
+        search_result = search(query, job_group, job_role, skills, max_month_pay, min_career_year, commute_type)
+        
+        response_data = {
+            "query": query,
+            "resumes": []
+        }
+        
+        for score, resume_id, comments in search_result:
+            resume = Resume.objects.get(id=resume_id)
+            senior_user = SeniorUser.objects.get(id=resume.user_id)
+            
+            response_data["resumes"].append({
+                "resume_id": resume_id,
+                "is_verified": resume.is_verified,
+                "career_year": resume.career_year,
+                "commute_type": resume.commute_type,
+                "profile_image": senior_user.profile_image,
+                "job_group": resume.job_group,
+                "job_role": resume.job_role,
+                "keyword": resume.keyword,
+                "skills": resume.skills,    
+                "score": score,
+                "comments": comments
+            })
+        
+        return Response(
+            data = response_data,
+            status=status.HTTP_200,
+        )
+    
+    
+    #@swagger_auto_schema(tags=['시니어 사용자 데이터를 생성합니다.'], request_body=SeniorSerializer)
+    def post(self, request):
+        # 검색 결과 객체 생성
+        response = self.create_search_result(data=request.data)
+        return response  
