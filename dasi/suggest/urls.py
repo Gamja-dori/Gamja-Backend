fe253faf740b54954a55e3b81a45c7470ffec67b
sforseohn <sforseohn@ewhain.net>
2024-05-20 02:41:00 +0900

    feat: add notifications api

--- dasi/suggest/urls.py
+++ dasi/suggest/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create/', SuggestCreateView.as_view(), name='create'),
-    path('enterprise/<int:user_id>/', GetSeniorListView.as_view(), name='senior_list'), 
-    path('senior/<int:user_id>/', GetEnterpriseListView.as_view(), name='enterprise_list'), 
-    path('<int:suggest_id>/', GetSuggestDetailView.as_view(), name='suggest_detail'),
-    path('pay/', PaymentRequestView.as_view(), name='payment_request'),
-    path('pay/approve/', PaymentApproveView.as_view(), name='payment_approve'),
-    path('pay/fail/', PaymentFailView.as_view(), name='payment_fail'),
-    path('pay/cancel/', PaymentCancelView.as_view(), name='payment_cancel'),
+    path('create/', SuggestCreateView.as_view()),
+    path('enterprise/<int:user_id>/', GetSeniorListView.as_view()), 
+    path('senior/<int:user_id>/read/', GetReadEnterpriseListView.as_view()), 
+    path('senior/<int:user_id>/unread/', GetUnreadEnterpriseListView.as_view()), 
+    path('<int:suggest_id>/', GetSuggestDetailView.as_view()),
+    path('notifications/<int:user_id>/', NotificationView.as_view()),
+    path('notifications/', NotificationView.as_view()),
+    path('pay/', PaymentRequestView.as_view()),
+    path('pay/approve/', PaymentApproveView.as_view()),
+    path('pay/fail/', PaymentFailView.as_view()),
+    path('pay/cancel/', PaymentCancelView.as_view()),
+    # path('pay/<int:suggest_id>/', GetIsPaidView.as_view()),    
 ]
-
